1fa3a505b5e942bffb8e20385cc0f40e7d659d79
JananiRS-git <mail2rsjanani@gmail.com>
2026-02-05 10:43:28 +0530

    Time: 0 ms (100.00%) | Memory: 43.3 MB (8.89%) - LeetSync

--- 24-swap-nodes-in-pairs/swap-nodes-in-pairs.java
+++ 24-swap-nodes-in-pairs/swap-nodes-in-pairs.java
@@ -8,7 +8,7 @@
  *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
  * }
  */
-class Solution {
+/*class Solution {
     public ListNode swapPairs(ListNode head) {
         ListNode dummy=new ListNode(0);
         dummy.next=head;
@@ -22,5 +22,19 @@ class Solution {
             prev=first;
         }
         return dummy.next;
+    }
+}*/
+
+class Solution {
+    public ListNode swapPairs(ListNode head) {
+        if(head==null || head.next==null) return head;
+        ListNode temp=head;
+        while(temp!=null && temp.next!=null){
+            int swap=temp.val;
+            temp.val=temp.next.val;
+            temp.next.val=swap;
+            temp=temp.next.next;
+        }
+        return head;
     }
 }
