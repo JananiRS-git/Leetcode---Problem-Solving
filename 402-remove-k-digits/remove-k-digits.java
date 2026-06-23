4620bcf74cc6148cc5ec00557b48701b1d3a6876
Janani R S <mail2rsjanani@gmail.com>
2026-06-23 20:14:13 +0530

    Time: 17 ms (75.57%) | Memory: 48 MB (22.89%) - LeetSync

--- 402-remove-k-digits/remove-k-digits.java
+++ 402-remove-k-digits/remove-k-digits.java
@@ -1,14 +1,11 @@
 public class Solution {
     public String removeKdigits(String num, int k) {
         int len = num.length();
-        //corner case
         if(k==len)        
             return "0";
-            
         Stack<Character> stack = new Stack<>();
         int i =0;
         while(i<num.length()){
-            //whenever meet a digit which is less than the previous digit, discard the previous one
             while(k>0 && !stack.isEmpty() && stack.peek()>num.charAt(i)){
                 stack.pop();
                 k--;
