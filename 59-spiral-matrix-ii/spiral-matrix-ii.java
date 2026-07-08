4190ff07874b5cfe7c4e94971436f6a32342131b
Janani R S <mail2rsjanani@gmail.com>
2026-07-08 09:35:16 +0530

    Time: 0 ms (100.00%) | Memory: 42.8 MB (62.55%) - LeetSync

---
+++ 59-spiral-matrix-ii/spiral-matrix-ii.java
@@ -0,0 +1,33 @@
+class Solution {
+    public int[][] generateMatrix(int n) {
+        int[][] res= new int[n][n];
+        int left=0;
+        int right=n-1;
+        int bottom=n-1;
+        int top=0;
+        int num=1;
+        while(left<=right && top<=bottom){
+            for(int j=left;j<=right;j++){
+                res[top][j]=num++;
+            }
+            top++;
+            for(int i=top;i<=bottom;i++){
+                res[i][right]=num++;
+            }
+            right--;
+            if(top<=bottom){
+                for(int j=right;j>=left;j--){
+                    res[bottom][j]=num++;
+                }
+                bottom--;
+            }
+            if(left<=right){
+                for(int i=bottom;i>=top;i--){
+                    res[i][left]=num++;
+                }
+                left++;
+            }
+        }
+        return res;
+    }
+}
